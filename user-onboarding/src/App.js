f8c71990da905205f9cb3e03ffd7e7b52a99a102
dwilliams304 <d.williams0434@gmail.com>
2023-08-14 10:36:55 -0700

    Finished

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,11 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
 import Form from './Components/Form';
+import React, {useState} from 'react';
+import formSchema from './Validation/FormSchema';
+import * as yup from 'yup';
+import axios from 'axios';
+
+const initialFormValues = {
+  username: '',
+  email: '',
+  password: '',
+  tos: false
+}
+
+const initialFormErrors = {
+  username: '',
+  email: '',
+  password: '',
+  tos: ''
+}
 
 function App() {
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [users, setUsers] = useState([]);
+
+  const submit = () => {
+    axios.post('https://reqres.in/api/users', formValues)
+      .then(res => {
+        setUsers([res.data, ...users]);
+      })
+      .catch(err => console.log(err))
+  }
+
+  const validate = (name, value) => {
+    yup.reach(formSchema, name)
+      .validate(value)
+      .then(() => {
+        setFormErrors({...formErrors, [name]: ''})
+      })
+      .catch(err => setFormErrors({...formErrors, [name]: err.errors[0]}))
+  }
+
+
+  const change = (name, value) => {
+    validate(name, value);
+    setFormValues({...formValues, [name]: value});
+  }
+
   return (
     <div className="App">
-      <Form />
+      <Form values={formValues} change={change} errors={formErrors} submit={submit}/>
+      
+        {
+        users.map(user => (
+          <div key={user.id}>
+            <h2>{user.username}</h2>
+            <p>{user.email}</p>
+            <p>{user.createdAt}</p>
+          
+          </div>
+        ))}
+
     </div>
   );
 }
--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -1,9 +1,68 @@
 import React from 'react'
 
-export default function Form(){
+export default function Form(props){
+    const {change, submit, errors} = props;
+    const {username, email, password, checked} = props.values;
+
+    const onChange = (evt) => {
+        const {name, value, checked, type} = evt.target;
+        const newVal = type === 'checkbox' ? checked: value;
+        change(name, newVal);
+    }
+
+    const onSubmit = (evt) => {
+        evt.preventDefault();
+        submit();
+    }
+
+
+
+
     return(
         <div>
-            
+            <h1>User Form</h1>
+            <p style={{'color': 'red'}}>{errors.username}</p>
+            <p style={{'color': 'red'}}>{errors.email}</p>
+            <p style={{'color': 'red'}}>{errors.password}</p>
+            <p style={{'color': 'red'}}>{errors.tos}</p>
+            <form onSubmit={onSubmit}>
+                <label>Name:
+                    <input 
+                        type='text'
+                        name='username'
+                        value={username}
+                        onChange={onChange}
+                    />
+                </label>
+
+                <label>Email:
+                    <input 
+                        type='email'
+                        name='email'
+                        value={email}
+                        onChange={onChange}
+                    />
+                </label>
+                
+                <label>Password:
+                    <input 
+                        type='password'
+                        name='password'
+                        value={password}
+                        onChange={onChange}
+                    />
+                </label>
+
+                <label>Terms of service:
+                    <input 
+                        type='checkbox'
+                        name='tos'
+                        checked={checked}
+                        onChange={onChange}
+                    />
+                </label>
+                <input type='submit' value='Create'/>
+            </form>
         </div>
     )
 }
